1c12f8cc83da0669399d7eca321b0612d7221229
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-02-05 20:49:31 +0600

    parcel create api create

---
+++ index.js
@@ -0,0 +1,58 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://mdmoniruzzamanarafat_db_user:${process.env.DB_PASSWORD}@cluster0.cvx7qwv.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+
+    const database = client.db('zapShift');
+    const parcelCollection = database.collection('parcels');
+
+    // parcel create api
+    app.post('/parcels', async (req, res) => {
+      try {
+        const parcel = req.body;
+        const result = await parcelCollection.insertOne(parcel);
+        res.status(201).json(result);
+      } catch (error) {
+        res.status(500).json({ error: err.message });
+      }
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('zap shift server');
+});
+
+app.listen(port, () => {
+  console.log('server is run.........');
+});
